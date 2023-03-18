8f8fa4daf56d6c5ef20220977d2bdf0f49247ee4
daanmgneni <94113582+daanmgneni@users.noreply.github.com>
2023-03-18 23:23:56 +0530

    Update program.cs

--- program.cs
+++ program.cs
@@ -5,7 +5,8 @@ namespace LineComparisionWithUseObj
         public static void Main(string[] args) 
         { 
         Console.WriteLine("Welcome to Line Comparison Computation Program");
-            
+            CalculateLength obj = new CalculateLength();
+            obj.check();
         }
     }
 }
